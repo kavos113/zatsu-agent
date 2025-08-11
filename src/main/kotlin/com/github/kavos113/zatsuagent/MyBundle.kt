c4976da633e03b7590444dd3ecfc15bf1fa48499
GitHub Action <action@github.com>
2025-08-11 11:41:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/kavos113/zatsuagent/MyBundle.kt
+++ src/main/kotlin/com/github/kavos113/zatsuagent/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kavos113.zatsuagent
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kavos113/zatsuagent/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/kavos113/zatsuagent/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.kavos113.zatsuagent.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
